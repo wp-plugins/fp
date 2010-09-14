01e7dbe4337607bb53fcc2016570d1b81aadc618
louyx <louyx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-14 01:23:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fp/tags/0.2@289033 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fp.php
+++ fp.php
@@ -173,8 +173,7 @@ function fp_section_text() {
 	$options = fp_options();
 	if (empty($options['api_key']) || empty($options['app_secret']) || empty($options['appid'])) {
 ?>
-<p><?php _e('To connect your site to Facebook, you will need a Facebook Application. 
-If you have already created one, please insert your API key, Application Secret, and Application ID below.', 'fp'); ?></p>
+<p><?php _e('To connect your site to Facebook, you will need a Facebook Application. If you have already created one, please insert your API key, Application Secret, and Application ID below.', 'fp'); ?></p>
 <p><strong><?php _e('Can&#39;t find your key?', 'fp'); ?></strong></p>
 <ol>
 <li><?php _e('Get a list of your applications from here: <a target="_blank" href="http://www.facebook.com/developers/apps.php">Facebook Application List</a>', 'fp'); ?></li>
@@ -186,8 +185,7 @@ If you have already created one, please insert your API key, Application Secret,
 <li><?php _e('Go to this link to create your application: <a target="_blank" href="http://developers.facebook.com/setup.php">Facebook Connect Setup</a>', 'fp'); ?></li>
 <li><?php _e('When it tells you to "Upload a file" on step 2, just hit the "Upload Later" button. This plugin takes care of that part for you!', 'fp'); ?></li>
 <li><?php _e('On the final screen, there will be an API Key field, in the yellow box. Copy and paste that information into here.', 'fp'); ?></li>
-<li><?php _e('You can get the rest of the information from the application on the 
-<a target="_blank" href="http://www.facebook.com/developers/apps.php">Facebook Application List</a> page.', 'fp'); ?></li>
+<li><?php _e('You can get the rest of the information from the application on the <a target="_blank" href="http://www.facebook.com/developers/apps.php">Facebook Application List</a> page.', 'fp'); ?></li>
 <li><?php _e('Select the application you want, then copy and paste the API key, Application Secret, and Application ID from there.', 'fp'); ?></li>
 </ol>
 <?php
